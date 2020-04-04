695b658987b2adf94ce53a1b128c1120bce9e5ce
mandaringit <mandarin.dev@kakao.com>
2020-04-04 22:37:06 +0900

    앱 패딩

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './pages/home/Main';
 import LoginContainer from './pages/auth/Login/LoginContainer';
 import Register from './pages/auth/Register';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  padding: 1rem;
+`;
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Main} exact />
-        <Route path="/login" component={LoginContainer} />
-        <Route path="/register" component={Register} />
+        <AppWrapper>
+          <Route path="/" component={Main} exact />
+          <Route path="/login" component={LoginContainer} />
+          <Route path="/register" component={Register} />
+        </AppWrapper>
       </Switch>
     </Router>
   );
