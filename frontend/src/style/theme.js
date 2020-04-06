a91a5f18fc91e653503ab4eee0eaee4f574597d2
mandaringit <mandarin.dev@kakao.com>
2020-04-06 15:26:26 +0900

    ADD Material Ui, + theme

---
+++ frontend/src/style/theme.js
@@ -0,0 +1,10 @@
+import { colors } from '@material-ui/core';
+
+const theme = {
+  mainColor: 'red',
+  mainBackgroundColor: colors.grey[100],
+  authFormWidth: '400px',
+  pageWidth: '1024px',
+};
+
+export default theme;
