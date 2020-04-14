9c9391a158dacc9611b3018e5d75b242d283e60e
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-04-14 11:09:12 +0900

    [+] accounts path 생성

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from accounts import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('accounts.urls')),
 ]
