14ed1a1cf26e8f3866bb7b3a9031741c475e9d05
hyeonahkiki <kkongc7@gmail.com>
2020-04-18 16:54:46 +0900

    [+]add HeadChart

--- frontend/src/pages/predict/HeaderBar/index.js
+++ frontend/src/pages/predict/HeaderBar/index.js
@@ -1,269 +1,31 @@
-import React from 'react';
-import { ResponsiveLine } from '@nivo/line';
+import { createChart } from 'lightweight-charts';
+import React, { useRef, useEffect } from 'react';
+// import styled from 'styled-components';
 
-//금지수
-const Idx = ({ data }) => (
-  <div style={{ height: '40vh', width: '50vh' }}>
-    <ResponsiveLine
-      data={[
-        {
-          id: 'Gold',
-          color: 'hsl(260, 70%, 50%)',
-          data: [
-            {
-              x: 'plane',
-              y: 154,
-            },
-            {
-              x: 'helicopter',
-              y: 121,
-            },
-            {
-              x: 'boat',
-              y: 129,
-            },
-          ],
-        },
-      ]}
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-      xScale={{ type: 'point' }}
-      yScale={{
-        type: 'linear',
-        min: 'auto',
-        max: 'auto',
-        stacked: true,
-        reverse: false,
-      }}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        orient: 'bottom',
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: '시간',
-        legendOffset: 36,
-        legendPosition: 'middle',
-      }}
-      axisLeft={{
-        orient: 'left',
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: '가격',
-        legendOffset: -40,
-        legendPosition: 'middle',
-      }}
-      colors={{ scheme: 'nivo' }}
-      pointSize={10}
-      pointColor={{ theme: 'background' }}
-      pointBorderWidth={2}
-      pointBorderColor={{ from: 'serieColor' }}
-      pointLabel="y"
-      pointLabelYOffset={-12}
-      useMesh={true}
-      legends={[
-        {
-          anchor: 'bottom-right',
-          direction: 'column',
-          justify: false,
-          translateX: 100,
-          translateY: 0,
-          itemsSpacing: 0,
-          itemDirection: 'left-to-right',
-          itemWidth: 80,
-          itemHeight: 20,
-          itemOpacity: 0.75,
-          symbolSize: 12,
-          symbolShape: 'circle',
-          symbolBorderColor: 'rgba(0, 0, 0, .5)',
-          effects: [
-            {
-              on: 'hover',
-              style: {
-                itemBackground: 'rgba(0, 0, 0, .03)',
-                itemOpacity: 1,
-              },
-            },
-          ],
-        },
-      ]}
-    />
-
-    {/* 유가지수 */}
-    <ResponsiveLine
-      data={[
-        {
-          id: 'Oil',
-          color: 'hsl(260, 70%, 50%)',
-          data: [
-            {
-              x: 'plane',
-              y: 154,
-            },
-            {
-              x: 'helicopter',
-              y: 121,
-            },
-            {
-              x: 'boat',
-              y: 129,
-            },
-          ],
-        },
-      ]}
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-      xScale={{ type: 'point' }}
-      yScale={{
-        type: 'linear',
-        min: 'auto',
-        max: 'auto',
-        stacked: true,
-        reverse: false,
-      }}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        orient: 'bottom',
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: '시간',
-        legendOffset: 36,
-        legendPosition: 'middle',
-      }}
-      axisLeft={{
-        orient: 'left',
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: '가격',
-        legendOffset: -40,
-        legendPosition: 'middle',
-      }}
-      colors={{ scheme: 'nivo' }}
-      pointSize={10}
-      pointColor={{ theme: 'background' }}
-      pointBorderWidth={2}
-      pointBorderColor={{ from: 'serieColor' }}
-      pointLabel="y"
-      pointLabelYOffset={-12}
-      useMesh={true}
-      legends={[
-        {
-          anchor: 'bottom-right',
-          direction: 'column',
-          justify: false,
-          translateX: 100,
-          translateY: 0,
-          itemsSpacing: 0,
-          itemDirection: 'left-to-right',
-          itemWidth: 80,
-          itemHeight: 20,
-          itemOpacity: 0.75,
-          symbolSize: 12,
-          symbolShape: 'circle',
-          symbolBorderColor: 'rgba(0, 0, 0, .5)',
-          effects: [
-            {
-              on: 'hover',
-              style: {
-                itemBackground: 'rgba(0, 0, 0, .03)',
-                itemOpacity: 1,
-              },
-            },
-          ],
-        },
-      ]}
-    />
-
-    {/* 구리지수 */}
-    <ResponsiveLine
-      data={[
-        {
-          id: 'Copper',
-          color: 'hsl(260, 70%, 50%)',
-          data: [
-            {
-              x: 'plane',
-              y: 154,
-            },
-            {
-              x: 'helicopter',
-              y: 121,
-            },
-            {
-              x: 'boat',
-              y: 129,
-            },
-          ],
-        },
-      ]}
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-      xScale={{ type: 'point' }}
-      yScale={{
-        type: 'linear',
-        min: 'auto',
-        max: 'auto',
-        stacked: true,
-        reverse: false,
-      }}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        orient: 'bottom',
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: '시간',
-        legendOffset: 36,
-        legendPosition: 'middle',
-      }}
-      axisLeft={{
-        orient: 'left',
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: '가격',
-        legendOffset: -40,
-        legendPosition: 'middle',
-      }}
-      colors={{ scheme: 'nivo' }}
-      pointSize={10}
-      pointColor={{ theme: 'background' }}
-      pointBorderWidth={2}
-      pointBorderColor={{ from: 'serieColor' }}
-      pointLabel="y"
-      pointLabelYOffset={-12}
-      useMesh={true}
-      legends={[
-        {
-          anchor: 'bottom-right',
-          direction: 'column',
-          justify: false,
-          translateX: 100,
-          translateY: 0,
-          itemsSpacing: 0,
-          itemDirection: 'left-to-right',
-          itemWidth: 80,
-          itemHeight: 20,
-          itemOpacity: 0.75,
-          symbolSize: 12,
-          symbolShape: 'circle',
-          symbolBorderColor: 'rgba(0, 0, 0, .5)',
-          effects: [
-            {
-              on: 'hover',
-              style: {
-                itemBackground: 'rgba(0, 0, 0, .03)',
-                itemOpacity: 1,
-              },
-            },
-          ],
-        },
-      ]}
-    />
-  </div>
-);
-
-export default Idx;
+function Chart(props) {
+  const chartdiv = useRef(null);
+  useEffect(() => {
+    const chart = createChart(chartdiv.current, { width: 500, height: 300 });
+    // chart size조정
+    chart.resize(300, 150);
+    const lineSeries = chart.addLineSeries();
+    lineSeries.setData([
+      { time: '2019-04-11', value: 80.01 },
+      { time: '2019-04-12', value: 96.63 },
+      { time: '2019-04-13', value: 76.64 },
+      { time: '2019-04-14', value: 81.89 },
+      { time: '2019-04-15', value: 74.43 },
+      { time: '2019-04-16', value: 80.01 },
+      { time: '2019-04-17', value: 96.63 },
+      { time: '2019-04-18', value: 76.64 },
+      { time: '2019-04-19', value: 81.89 },
+      { time: '2019-04-20', value: 74.43 },
+    ]);
+  }, []);
+  return (
+    <div>
+      <div ref={chartdiv} />
+    </div>
+  );
+}
+export default Chart;
