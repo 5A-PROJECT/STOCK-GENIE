4d6c6a915827db4a210259b2815a9c7076fc800c
Jw <betrayers000@gmail.com>
2020-04-23 12:02:07 +0900

    [+] add stockinfo model

--- backend/predict/models.py
+++ backend/predict/models.py
@@ -2,3 +2,18 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+
+class StockInfo(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=20)
+    open = models.FloatField()
+    close = models.FloatField()
+    predict = models.IntegerField()
+
+    @property
+    def rate(self):
+        return round(((self.close - self.open)/self.open) * 100, 2)
+
+    def __str__(self):
+        return self.name
