8c8828c47b2b80d01c5adcc53a20e380dd54d818
Jw <betrayers000@gmail.com>
2020-04-23 12:02:18 +0900

    [+] stockinfo api add

--- backend/predict/urls.py
+++ backend/predict/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('commodities/', views.commodities),
     path('indices/', views.indices),
     path('currencycross/', views.currency_cross),
+    path('stocktable/', views.stock_table)
 ]
