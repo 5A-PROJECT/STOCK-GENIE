bb20b3fe1107f72193b7a63f6e8dbb276cec8afe
mandaringit <mandarin.dev@kakao.com>
2020-04-23 11:17:28 +0900

    현아 컴포넌트 다시 추가

--- frontend/src/pages/predict/index.js
+++ frontend/src/pages/predict/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import AccessProtection from '../../molecules/AccessProtection';
+import CompanyTable from '../predict/CompanyTable/index';
+import HeadCharts from './HeadCharts';
+import MainChart from './MainChart';
 
 const PredictPageWrapper = styled.div`
   max-width: ${({ theme }) => theme.width.page};
   margin: 0 auto;
+  padding: 0 1rem;
 `;
 
 function PredictPage(props) {
   return (
     <AccessProtection authed={true} redirectPath={'/login'}>
       <PredictPageWrapper>
-        <div>주가예측 페이지</div>
+        <HeadCharts />
+        <MainChart />
+        <CompanyTable />
       </PredictPageWrapper>
     </AccessProtection>
   );
