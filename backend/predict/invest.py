f8a251ef48aa36de2808ab063b89be2c7a2e8c47
mandaringit <mandarin.dev@kakao.com>
2020-04-23 11:36:23 +0900

    백엔드 복구

--- backend/predict/invest.py
+++ backend/predict/invest.py
@@ -3,6 +3,9 @@ import json
 
 KOREA = 'south korea'
 US = 'united states'
+KRW = "KRW"
+USD = "USD"
+
 
 FAIL = json.dumps({"result": "FAIL"})
 SUCCESS = json.dumps({"result": "SUCCESS"})
@@ -12,13 +15,43 @@ def get_commodities(name, from_date, to_date):
     if name not in investpy.commodities.get_commodities_list():
         return FAIL
     data = investpy.commodities.get_commodity_historical_data(
-        name, from_date=from_date, to_date=to_date, as_json=True)
-    return data
+        name, from_date=from_date, to_date=to_date)
+    data = data.drop(['Open', 'High', 'Low', 'Volume', 'Currency'], axis=1)
+    data.rename(columns={'Close': 'value'}, inplace=True)
+    data['time'] = data.index.map(lambda x: str(x).split(" ")[0])
+    result = {}
+    result['data'] = data.to_dict('records')
+    result = json.dumps(result)
+    # result = {"data": []}
+    # for i in range(len(data)):
+    #     time = str(data.index[i]).split(" ")[0]
+    #     value = data["Close"][i]
+    #     result["data"].append({"time": time, "value": value})
+    # result = json.dumps(result)
+    return result
 
 
 def get_indices(name, country, from_date, to_date):
     if name not in investpy.indices.get_indices_list(country):
         return FAIL
     data = investpy.indices.get_index_historical_data(
-        name, from_date=from_date, to_date=to_date, country=country, as_json=True)
-    return data
+        name, from_date=from_date, to_date=to_date, country=country)
+    data = data.drop(['Open', 'High', 'Low', 'Volume', 'Currency'], axis=1)
+    data.rename(columns={'Close': 'value'}, inplace=True)
+    data['time'] = data.index.map(lambda x: str(x).split(" ")[0])
+    result = {}
+    result['data'] = data.to_dict('records')
+    result = json.dumps(result)
+    return result
+
+
+def get_currency_cross(currency_cross, from_date, to_date):
+    data = investpy.get_currency_cross_historical_data(
+        currency_cross=currency_cross, from_date=from_date, to_date=to_date)
+    data = data.drop(['Open', 'High', 'Low', 'Currency'], axis=1)
+    data.rename(columns={'Close': 'value'}, inplace=True)
+    data['time'] = data.index.map(lambda x: str(x).split(" ")[0])
+    result = {}
+    result['data'] = data.to_dict('records')
+    result = json.dumps(result)
+    return result
