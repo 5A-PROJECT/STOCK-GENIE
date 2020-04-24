105acf762fafa2414d517c5f5692044dc91e87ba
mandaringit <mandarin.dev@kakao.com>
2020-04-24 16:18:14 +0900

    [*] 파이차트 모양 수정

--- frontend/src/pages/portfolioItem/RatePieChart/index.js
+++ frontend/src/pages/portfolioItem/RatePieChart/index.js
@@ -13,12 +13,12 @@ function RatePieChart({ data, scheme = 'nivo' }) {
         data={data}
         margin={{ top: 0, right: 80, bottom: 80, left: 80 }}
         innerRadius={0.5}
-        padAngle={0.7}
+        padAngle={3}
         cornerRadius={3}
         colors={{ scheme }}
         borderWidth={2}
         borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
-        radialLabel={(d) => `${d.id} (%)`}
+        radialLabel={(d) => `${d.id} (${d.value}%)`}
         radialLabelsSkipAngle={10}
         radialLabelsTextXOffset={6}
         radialLabelsTextColor="#333333"
