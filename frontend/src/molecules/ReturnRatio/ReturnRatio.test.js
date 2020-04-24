059cf10560f0afcdb5a220864b94f9f5f4b8b9d4
mandaringit <mandarin.dev@kakao.com>
2020-04-24 09:21:41 +0900

    [*] ReturnRatio 컴포넌트 소숫점 아래 오류 렌더링 수정

--- frontend/src/molecules/ReturnRatio/ReturnRatio.test.js
+++ frontend/src/molecules/ReturnRatio/ReturnRatio.test.js
@@ -10,14 +10,14 @@ describe('<ReturnRatio />', () => {
     expect(container).toMatchSnapshot();
   });
   it('음수 수익률은 DownIcon과 -{ratio}% 가 렌더링 되어야 합니다.', () => {
-    const { getByText, getByTitle } = Sample(-3.5);
+    const { getByText, getByTitle } = Sample(-0.5);
     getByTitle('down');
-    getByText('-3.5%');
+    getByText('-0.5%');
   });
   it('양수 수익률은 UpIcon 과 {ratio}% 가 렌더링 되어야 합니다.', () => {
-    const { getByText, getByTitle } = Sample(55.5);
+    const { getByText, getByTitle } = Sample(0.5);
     getByTitle('up');
-    getByText('55.5%');
+    getByText('0.5%');
   });
 
   it('0의 수익률은 StableIcon과 0% 가 렌더링 되어야 합니다.', () => {
