dfa931c7adc385c94ff82421f72a3642666310d7
hyeonahkiki <kkongc7@gmail.com>
2020-04-25 23:10:35 +0900

    [+] add CompanyTable

--- frontend/src/pages/predict/MainChart/index.js
+++ frontend/src/pages/predict/MainChart/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Chart from '../Chart';
+import PaginationTable from '../CompanyTable';
+import { Table } from '@material-ui/core';
 
 const MainChartWrapper = styled.div`
   display: grid;
@@ -9,6 +11,8 @@ const MainChartWrapper = styled.div`
 `;
 
 const SelectorWrapper = styled.div``;
+
+const TableWrapper = styled.div``;
 
 const categories = {
   'south korea': ['KOSPI', 'KOSDAQ'],
@@ -30,43 +34,54 @@ function MainChart() {
   };
 
   return (
-    <MainChartWrapper>
-      {/* 셀렉터 */}
-      <SelectorWrapper>
-        <h2>나라 선택</h2>
-        <select value={nation} onChange={onNationChange}>
-          <option value="">나라를 선택하세요.</option>
-          <option value="south korea">대한민국</option>
-          <option value="united states">미국</option>
-        </select>
+    <>
+      <MainChartWrapper>
+        {/* 셀렉터 */}
+        <SelectorWrapper>
+          <h2>나라 선택</h2>
+          <select value={nation} onChange={onNationChange}>
+            <option value="">나라를 선택하세요.</option>
+            <option value="south korea">대한민국</option>
+            <option value="united states">미국</option>
+          </select>
 
-        {/* 나라 선택 시에만 보이도록 */}
-        {nation && (
-          <>
-            <h2>종목선택</h2>
-            <select value={category} onChange={onCategoryChange}>
-              <option value="">종목을 선택하세요.</option>
-              {categories[nation].map((cat, idx) => (
-                <option value={cat} key={idx}>
-                  {cat}
-                </option>
-              ))}
-            </select>
-          </>
-        )}
-      </SelectorWrapper>
+          {/* 나라 선택 시에만 보이도록 */}
+          {nation && (
+            <>
+              <h2>종목선택</h2>
+              <select value={category} onChange={onCategoryChange}>
+                <option value="">종목을 선택하세요.</option>
+                {categories[nation].map((cat, idx) => (
+                  <option value={cat} key={idx}>
+                    {cat}
+                  </option>
+                ))}
+              </select>
+            </>
+          )}
+        </SelectorWrapper>
 
-      {/* 차트 */}
-      <Chart
-        width="600"
-        height="300"
-        url="indices"
-        params={{
-          country: nation,
-          name: category,
-        }}
-      />
-    </MainChartWrapper>
+        {/* 차트 */}
+        <Chart
+          width="600"
+          height="300"
+          url="indices"
+          params={{
+            country: nation,
+            name: category,
+          }}
+        />
+      </MainChartWrapper>
+
+      {/* 회사정보 테이블 */}
+      <TableWrapper>
+        <PaginationTable
+          params={{
+            index: category,
+          }}
+        />
+      </TableWrapper>
+    </>
   );
 }
 
