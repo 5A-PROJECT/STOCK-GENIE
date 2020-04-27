9285cfe279b09d7234a514ea37188b06c374c121
mandaringit <mandarin.dev@kakao.com>
2020-04-27 19:42:09 +0900

    [+] 메인페이지 검색창 생성

--- frontend/src/pages/home/Main/SearchInput/index.js
+++ frontend/src/pages/home/Main/SearchInput/index.js
@@ -1,5 +1,47 @@
 import React, { useState } from 'react';
-import MaterialInput from '../../../../atoms/Input/MaterialInput';
+import { TextField, StylesProvider, Button } from '@material-ui/core';
+import styled from 'styled-components';
+
+const CustomInput = styled(TextField)`
+  display: block;
+  background-color: white;
+  & + & {
+    margin-top: 1rem;
+  }
+  /* 기본 보더 색상 */
+  & .MuiOutlinedInput-notchedOutline {
+    border-color: ${({ theme }) => theme.color.materialInput.border};
+  }
+  /* 호버 시 보더 색상 */
+  &:hover .MuiOutlinedInput-notchedOutline {
+    border-color: ${({ theme }) => theme.color.materialInput.hoverBorder};
+  }
+  /* 포커스시 라벨 색상 */
+  & label.Mui-focused {
+    color: ${({ theme }) => theme.color.materialInput.label};
+  }
+  /* 포커스시 보더 색상 */
+  & .Mui-focused .MuiOutlinedInput-notchedOutline {
+    border-color: ${({ theme }) => theme.color.materialInput.focusedBorder};
+  }
+`;
+
+const CustomButton = styled(Button)`
+  & + & {
+    margin-left: 0.3rem;
+  }
+  color: white;
+  background-color: ${({ theme }) => theme.color.materialButton.background};
+  :hover {
+    background-color: ${({ theme }) =>
+      theme.color.materialButton.hoverBackground};
+  }
+`;
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+`;
 
 function SearchInput(props) {
   const [query, setQuery] = useState('');
@@ -8,7 +50,29 @@ function SearchInput(props) {
     setQuery(e.target.value);
   };
 
-  return <MaterialInput value={query} onChange={onInputChange} />;
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(query);
+  };
+
+  return (
+    <>
+      <StylesProvider injectFirst>
+        <Form onSubmit={onSubmit}>
+          <CustomInput
+            {...props}
+            variant="outlined"
+            fullWidth
+            value={query}
+            onChange={onInputChange}
+          />
+          <CustomButton {...props} variant="contained">
+            검색
+          </CustomButton>
+        </Form>
+      </StylesProvider>
+    </>
+  );
 }
 
 export default SearchInput;
--- frontend/src/pages/home/Main/index.js
+++ frontend/src/pages/home/Main/index.js
@@ -4,20 +4,43 @@ import SearchInput from './SearchInput';
 
 const MainPageWrapper = styled.div`
   height: 100%;
-  background-image: url(${`${process.env.PUBLIC_URL}/main_background.jpg`});
-  background-position: center;
-  background-size: cover;
+  width: 100%;
+  position: relative;
+  ::before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    z-index: -1;
+    opacity: 0.3;
+    background-image: url(${`${process.env.PUBLIC_URL}/main_background.jpg`});
+    background-position: center;
+    background-size: cover;
+  }
+`;
+
+const Logo = styled.div`
+  font-size: 6rem;
 `;
 
 const Contents = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   padding: 2rem;
+  height: 100%;
+  max-width: 1024px;
+  margin: 0 auto;
 `;
 
 function MainPage() {
   return (
     <MainPageWrapper>
       <Contents>
-        <div>5A</div>
+        <Logo>로고 플리즈</Logo>
         <SearchInput />
       </Contents>
     </MainPageWrapper>
