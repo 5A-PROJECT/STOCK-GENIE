b88f13ea78ff1e544e846a548c85ecf3c1e48a93
hyeonahkiki <kkongc7@gmail.com>
2020-04-28 00:46:31 +0900

    [+] modify IndexList

--- frontend/src/pages/predict/IndexList/index.js
+++ frontend/src/pages/predict/IndexList/index.js
@@ -22,7 +22,7 @@ const DownIcon = styled(ArrowDropDownIcon)`
 
 // 상세보기 버튼
 const ZoomIcon = styled(ZoomInIcon)`
-  color: navy;
+  color: ${colors.grey[600]};
   font-size: ${(props) => props.size};
 `;
 
@@ -33,11 +33,81 @@ const TableWrapper = styled.div`
   grid-template-rows: auto;
 `;
 
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  height: 3rem;
+  border-bottom: 1px solid #9e9e9e;
+  border-top: 1px solid #9e9e9e;
+  background-color: ${colors.teal[50]};
+  box-shadow: 2px 2px 1px 1px ${colors.grey[300]};
+
+  .start {
+    width: 30%;
+    font-size: 1.2rem;
+    font-weight: bold;
+    color: ${colors.teal[900]};
+  }
+  .title {
+    width: 20%;
+    font-size: 1.2rem;
+    font-weight: bold;
+    color: ${colors.teal[900]};
+  }
+
+  .end {
+    width: 10%;
+    justify-self: flex-end;
+    font-size: 1.2rem;
+    font-weight: bold;
+    align-self: center;
+    text-align: end;
+    color: ${colors.teal[900]};
+  }
+`;
+
 const StockWrapper = styled.div`
   display: flex;
+  justify-content: space-between;
   border: 1px solid ${colors.grey[300]};
   padding: 1rem;
   border-radius: 5px;
+  align-items: center;
+  font-size: 1rem;
+  box-shadow: 2px 2px 1px 1px ${colors.grey[300]};
+
+  cursor: pointer;
+  :hover {
+    transition-duration: 0.3s;
+    background-color: ${colors.grey[200]};
+    transform: scale(1.03);
+  }
+
+  .name {
+    flex-wrap: wrap;
+    font-weight: bold;
+  }
+  .close {
+    width: 20%;
+  }
+  .open {
+    width: 20%;
+  }
+  .rate {
+    width: 20%;
+  }
+  .pagemovebtn {
+    justify-self: flex-end;
+    align-self: center;
+    width: 10%;
+    text-align: end;
+  }
+`;
+const Div = styled.div`
+  display: flex;
+  width: 30%;
 `;
 
 function IndexList({ index }) {
@@ -68,28 +138,41 @@ function IndexList({ index }) {
 
   return (
     <TableWrapper>
+      <Header>
+        <div className="start">이름(코드)</div>
+        <div className="title">전일종가</div>
+        <div className="title">시가</div>
+        <div className="title">등략율</div>
+        <div className="end">상세보기</div>
+      </Header>
       {data.map((d) => (
         <StockWrapper key={d.id}>
-          <>{d.predictpoint === 1 ? <UpIcon /> : <DownIcon />}</>
-          <div>{d.name}</div>
-          <div>{d.code}</div>
-          <div>{d.country}</div>
-          <div>{d.open}</div>
-          <div>{d.rate}</div>
-          <Link
-            to={{
-              pathname: 'stockdetail',
-              state: {
-                name: d.name,
-                code: d.code,
-                country: d.country,
-                currentprice: d.open,
-                index: index.toUpperCase(),
-              },
-            }}
-          >
-            <ZoomIcon size={'1.5rem'} title="detail" />
-          </Link>
+          <Div>
+            <div className="icon">
+              {d.predictpoint === 1 ? <UpIcon /> : <DownIcon />}
+            </div>
+            <div className="name">{d.name}</div>
+            {/* <div className="code">({d.code})</div> */}
+          </Div>
+          <div className="close">{d.close.toLocaleString()}</div>
+          <div className="open">{d.open.toLocaleString()}</div>
+          <div className="rate">{d.rate}</div>
+          <div className="pagemovebtn">
+            <Link
+              to={{
+                pathname: 'stockdetail',
+                state: {
+                  name: d.name,
+                  code: d.code,
+                  country: d.country,
+                  currentprice: d.open,
+                  index: index.toUpperCase(),
+                },
+              }}
+            >
+              <ZoomIcon size={'1.5rem'} title="detail" />
+            </Link>
+          </div>
         </StockWrapper>
       ))}
     </TableWrapper>
