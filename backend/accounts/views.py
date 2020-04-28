5a39f2a599537a5ec90047ae87a5593b107f5922
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-04-28 10:48:41 +0900

    S02P23C101-120 [*] accounts 기능 최적화

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -8,8 +8,6 @@ from rest_framework_jwt.settings import api_settings
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from .serializers import UserSerializer
 
-# Create your views here.
-
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
@@ -20,26 +18,22 @@ jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
 def register(request):
     global jwt_payload_handler, jwt_encode_handler
 
-    # validation username conflict
+    # validation username
     username = request.data.get('username')
     users = get_user_model().objects.filter(username__exact=username)
     if len(users) > 0:
         return HttpResponse(status=409)
 
-    # password validation will be needed
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
         password = request.data.get('password')
         user = serializer.save()
         user.set_password(password)
         user.save()
-
         payload = jwt_payload_handler(user)
         token = jwt_encode_handler(payload)
+        return JsonResponse({'token': token, 'id': user.id})
 
-        user_id = user.id
-
-        return JsonResponse({'token': token, 'id': user_id})
     return HttpResponse(status=400)
 
 
@@ -55,10 +49,8 @@ def login(request):
     if check_password(password, user.password) == True:
         payload = jwt_payload_handler(user)
         token = jwt_encode_handler(payload)
+        return JsonResponse({'token': token, 'id': user.id})
 
-        user_id = user.id
-
-        return JsonResponse({'token': token, 'id': user_id})
     return HttpResponse(status=400)
 
 
@@ -72,9 +64,5 @@ def check(request):
     if token == None:
         return HttpResponse(status=401)
 
-    token = token.split(' ')[-1]
-    user = jwt_decode_handler(token)
-
-    user_id = user.get('user_id')
-    username = user.get('username')
-    return JsonResponse({'id': user_id, 'username': username})
+    user = jwt_decode_handler(token.split(' ')[-1])
+    return JsonResponse({'id': user.get('user_id'), 'username': user.get('username')})
