a026a535732b37c343e64c2eba731924ab88e753
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-04-28 10:01:21 +0900

    S02P23C101-113 [*] cron currency 자동화 API 체크

--- backend/stock/admin.py
+++ backend/stock/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Stock
+from .models import Stock, Currency
 
 # Register your models here.
 
 admin.site.register(Stock)
+admin.site.register(Currency)
